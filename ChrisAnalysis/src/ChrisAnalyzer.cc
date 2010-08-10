6fe68c83fb296e5302dc9ea9d75d6ab08916db59
crsilk <>
2010-08-10 15:42:48 +0000

    Added many histograms as well as a couple other functions.

--- ChrisAnalysis/src/ChrisAnalyzer.cc
+++ ChrisAnalysis/src/ChrisAnalyzer.cc
@@ -39,31 +39,15 @@ void ChrisAnalyzer::initialize(const edm::ParameterSet& ps,
 			      TFileDirectory& aDir,
 			      pat::strbitset *aSelections) {
   pt_= 0;   
-  nTruePhotons_ = 0.0;
-  vector<double> tmp(32);
-  for(int i = 0; i < 32; i++)
-    {
-      nInCutKL0s_.push_back(0);
-      nOutCutKL0s_.push_back(0);
-      nInCutEles_.push_back(0);
-      nOutCutEles_.push_back(0);
-      nInCutPions_.push_back(0);
-      nOutCutPions_.push_back(0);
-      inCutPt_.push_back(tmp);
-      nInCutPhotons_.push_back(tmp);
-      nOutCutPhotons_.push_back(tmp);
-      totPure_.push_back(tmp);
-    }
+  nTruePhotons_ = 0;
   
-   
-
-   verbose_ = ps.getParameter<bool>("verbose");
-   pfCandidateLabel_ = ps.getParameter<edm::InputTag>("pfCandidateLabel");
-   genParticleLabel_ = ps.getParameter<edm::InputTag>("genParticleLabel");
-   photonLabel_ = ps.getParameter<edm::InputTag>("photonLabel");
-   chrisHistos_ = new ChrisHistograms(&aDir,"Photons");
-    
-   registerCuts();
+  verbose_ = ps.getParameter<bool>("verbose");
+  pfCandidateLabel_ = ps.getParameter<edm::InputTag>("pfCandidateLabel");
+  genParticleLabel_ = ps.getParameter<edm::InputTag>("genParticleLabel");
+  photonLabel_ = ps.getParameter<edm::InputTag>("photonLabel");
+  chrisHistos_ = new ChrisHistograms(&aDir,"Photons");
+  
+  registerCuts();
 
 }
 
@@ -84,30 +68,23 @@ void ChrisAnalyzer::registerCuts(){
 bool ChrisAnalyzer::analyze(const edm::EventBase& iEvent){
 
   if(verbose_) std::cout<<iEvent.id()<<std::endl;  
-  
-  //vector<double> delR;
-  //vector<double> delP;
-  
-  //double nMatchedToGenPho1;
-  //double nMatchedToGenPho2;
+
   int k =0;					
   int i = 0;
+  double inConePt;
+
   vector<int> inConePhotons(2);
   vector<int> inConeElectrons(2);
   vector<int> inConePions(2);
   vector<int> inConeKL0s(2);
   vector<int> inConePFParticles(2);
+  vector<int> inConeRecoPhotons(2);
   vector<double> inConeTotPhotonPt(2);
   vector<double> inConeTotElectronPt(2);
   vector<double> inConeTotPionPt(2);
   vector<double> inConeTotKL0Pt(2);
   vector<double> inConeTotPFParticlePt(2);
-  
-  //for(int i2 = 0; i2<16; i2++)
-  // {
-  //   delR.push_back(.05*(i2+1));
-  //   delP.push_back(.75*(i2+1));
-  //  }
+  vector<double> inConeTotRecoPhotonPt(2);
  
   edm::Handle<PFCandidateCollection> pfHandle;
   iEvent.getByLabel( pfCandidateLabel_, pfHandle );
@@ -119,6 +96,7 @@ bool ChrisAnalyzer::analyze(const edm::EventBase& iEvent){
     
   edm::Handle<PhotonCollection> photonHandle;
   iEvent.getByLabel(photonLabel_,photonHandle);
+  PhotonCollection::const_iterator recoPhoton;
   
   pt_ = genHandle->begin()->pt();
   
@@ -126,19 +104,29 @@ bool ChrisAnalyzer::analyze(const edm::EventBase& iEvent){
   double includeAllPtCut = 50;
   double delRCut = .7;  
   double delPCut = pt_/3; 
-
   
-  for(genParticle = genHandle->begin(); genParticle != genHandle->end(); genParticle++)
+  
+  for(genParticle = genHandle->begin(); 
+      genParticle != genHandle->end(); genParticle++)
     {
-      for(pfParticle = pfHandle->begin(); pfParticle != pfHandle->end(); pfParticle++)
+      inConePt = 0;
+      
+      //Sums up the total in cone pt.
+      for(pfParticle = pfHandle->begin(); 
+	  pfParticle != pfHandle->end(); pfParticle++)      
+	{
+	  if(isMatched(pfParticle, genParticle, delRCut, includeAllPtCut))
+	    inConePt += pfParticle->pt();
+	}
+
+      for(pfParticle = pfHandle->begin(); 
+	  pfParticle != pfHandle->end(); pfParticle++)
 	{ 
 	  //All PF particles
 	  if(isMatched(pfParticle, genParticle, delRCut, includeAllPtCut))
 	    {
 	      inConePFParticles[k] ++;
 	      inConeTotPFParticlePt[k] += pfParticle->pt();
-	      chrisHistos_->fill1DHistogram("inConePFParticlePt", 
-					    pfParticle->pt());
 	      chrisHistos_->fill2DHistogram("inConePFParticleDeltaAngle",
 					    pfParticle->eta()-genParticle->eta(),
 					    pfParticle->phi()-genParticle->phi());
@@ -147,40 +135,86 @@ bool ChrisAnalyzer::analyze(const edm::EventBase& iEvent){
 	  //Photons
 	  if(pfParticle->pdgId() == 22)
 	    {
-	      if(isMatched(pfParticle, genParticle, delRCut, delPCut))
+	      if(isMatched(pfParticle, genParticle, delRCut, includeAllPtCut))
 		{
 		  inConePhotons[k]++;
 		  inConeTotPhotonPt[k] += pfParticle->pt();
-		  chrisHistos_->fill1DHistogram("inConePhotonPt", 
-					    pfParticle->pt());
+		  if(inConePt > 38.0)
+		    {
+		      chrisHistos_->fill1DHistogram("photonOverMeasured",
+						    pfParticle->pt());
+		    }
+	
+		  if( inConePt < 38.0 && inConePt > 32.0)
+		    {
+		      chrisHistos_->fill1DHistogram("photonCorrectlyMeasured",
+						    pfParticle->pt());
+		    }
+	
+		  if(inConePt < 40.0)
+		    {
+		      chrisHistos_->fill1DHistogram("photonUnderMeasured",
+						  pfParticle->pt());
+		    }
+		  
 		  chrisHistos_->fill2DHistogram("inConePhotonDeltaAngle",
 						pfParticle->eta()-genParticle->eta(),
 						pfParticle->phi()-genParticle->phi());
 		}       
 	    }
 	  //Electrons
-	  if(pfParticle->pdgId() == abs(11))
+	  if(abs(pfParticle->pdgId()) == 11)
 	    {
 	      if(isMatched(pfParticle, genParticle, delRCut, includeAllPtCut))
 		{
 		  inConeElectrons[k]++;
 		  inConeTotElectronPt[k] += pfParticle->pt();
-		  chrisHistos_->fill1DHistogram("inConeElectronPt", 
-					    pfParticle->pt());
-		  chrisHistos_->fill2DHistogram("inConeElectronDeltaAngle",
+		  if(inConePt > 38.0)
+		    {
+		      chrisHistos_->fill1DHistogram("electronOverMeasured",
+						    pfParticle->pt());
+		    }
+		  
+		    if(inConePt > 32.0 && inConePt < 38.0)
+		      {
+			chrisHistos_->fill1DHistogram("electronCorrectlyMeasured",
+						      pfParticle->pt());
+		      }
+		  
+		  if(inConePt < 40.0)
+		    {
+		      chrisHistos_->fill1DHistogram("electronUnderMeasured",
+						    pfParticle->pt());
+		    }
+		    chrisHistos_->fill2DHistogram("inConeElectronDeltaAngle",
 						pfParticle->eta()-genParticle->eta(),
 						pfParticle->phi()-genParticle->phi());
 		}
 	    }
 	  //Pions
-	  if(pfParticle->pdgId() == abs(211))
+	  if(abs(pfParticle->pdgId()) == 211)
 	    {
 	      if(isMatched(pfParticle, genParticle, delRCut, includeAllPtCut))
 		{
 		  inConePions[k]++;
 		  inConeTotPionPt[k] += pfParticle->pt();
-		  chrisHistos_->fill1DHistogram("inConePionPt", 
-						pfParticle->pt());
+		  if(inConePt > 38.0)
+		    {
+		      chrisHistos_->fill1DHistogram("pionOverMeasured",
+						    pfParticle->pt());
+		    }
+
+		  if(inConePt > 32.0 && inConePt < 38.0)
+		    {
+		      chrisHistos_->fill1DHistogram("pionCorrectlyMeasured",
+						    pfParticle->pt());
+		    }
+
+		  if(inConePt < 40.0)
+		    {
+		      chrisHistos_->fill1DHistogram("pionUnderMeasured",
+						    pfParticle->pt());
+		    }
 		  chrisHistos_->fill2DHistogram("inConePionDeltaAngle",
 						pfParticle->eta()-genParticle->eta(),
 						pfParticle->phi()-genParticle->phi());
@@ -193,100 +227,89 @@ bool ChrisAnalyzer::analyze(const edm::EventBase& iEvent){
 		{
 		  inConeKL0s[k]++;
 		  inConeTotKL0Pt[k] += pfParticle->pt();
-		  chrisHistos_->fill1DHistogram("inConeKL0Pt", 
-						pfParticle->pt());
+		  if(inConePt > 38.0)
+		    {
+		      chrisHistos_->fill1DHistogram("KL0OverMeasured",
+						    pfParticle->pt());
+		    }
+		  
+		  if(inConePt > 32.0 && inConePt < 38.0)
+		    {
+		      chrisHistos_->fill1DHistogram("KL0CorrectlyMeasured",
+						    pfParticle->pt());
+		    }
+		  
+		  if(inConePt < 40.0)
+		    {
+		      chrisHistos_->fill1DHistogram("KL0UnderMeasured",
+						    pfParticle->pt());
+		    }
 		  chrisHistos_->fill2DHistogram("inConeKL0DeltaAngle",
 					    pfParticle->eta()-genParticle->eta(),
 					    pfParticle->phi()-genParticle->phi());
 		}
 	    }
 	}
+      //Go through the reco photons
+      for(recoPhoton = photonHandle->begin(); 
+	  recoPhoton != photonHandle->end(); recoPhoton++)
+	{
+	  if(isMatched(recoPhoton, genParticle, delRCut, includeAllPtCut))
+	    {
+	      inConeRecoPhotons[k]++;
+	      inConeTotRecoPhotonPt[k] += recoPhoton->pt();
+	    }
+	}
       k++;
     }
 
   //fill the histograms
-  for(genParticle = genHandle->begin(); genParticle != genHandle->end(); genParticle++)
+  for(genParticle = genHandle->begin(); 
+      genParticle != genHandle->end(); genParticle++)
     {
       chrisHistos_->fill1DHistogram("inConePhotons", inConePhotons[i]);
       chrisHistos_->fill1DHistogram("inConeElectrons", inConeElectrons[i]);
       chrisHistos_->fill1DHistogram("inConePions", inConePions[i]);
       chrisHistos_->fill1DHistogram("inConeKL0s", inConeKL0s[i]);
       chrisHistos_->fill1DHistogram("inConePFParticles", inConePFParticles[i]);
-
-
-      chrisHistos_->fill1DHistogram("inConeTotPhotonPt", inConeTotPhotonPt[i]);
-      chrisHistos_->fill1DHistogram("inConeTotElectronPt", inConeTotElectronPt[i]);
-      chrisHistos_->fill1DHistogram("inConeTotPionPt", inConeTotPionPt[i]);
-      chrisHistos_->fill1DHistogram("inConeTotKL0Pt", inConeTotKL0Pt[i]);
-      chrisHistos_->fill1DHistogram("inConeTotPFParticlePt", inConeTotPFParticlePt[i]);
+      chrisHistos_->fill1DHistogram("inConeRecoPhotons", inConeRecoPhotons[i]);
+      if(inConePhotons[i] != 0) 
+	chrisHistos_->fill1DHistogram("inConeTotPhotonPt", 
+				      inConeTotPhotonPt[i]);
+      if(inConeElectrons[i] != 0)
+	chrisHistos_->fill1DHistogram("inConeTotElectronPt", 
+				      inConeTotElectronPt[i]);
+      if(inConePions[i] !=0)
+	chrisHistos_->fill1DHistogram("inConeTotPionPt", 
+				      inConeTotPionPt[i]);
+      if(inConeKL0s[i] !=0)
+	chrisHistos_->fill1DHistogram("inConeTotKL0Pt", 
+				      inConeTotKL0Pt[i]);
+      if(abs(genParticle->eta()) < 0.3)
+	{
+	  if(inConePFParticles[i] !=0)
+	    chrisHistos_->fill1DHistogram("inConeTotPFParticlePt", 
+					  inConeTotPFParticlePt[i]);
+	  if(inConeRecoPhotons[i] != 0)
+	    chrisHistos_->fill1DHistogram("inConeTotRecoPhotonPt", 
+					  inConeTotRecoPhotonPt[i]);
+	}
       i++;
     }
 
 
-
-
-
-  
-  //Code for getting all the plots for efficiency, 'purity', delta R, delta
-  //eta, delta phi and delta pt for True photon pt = 35.
-
-
-  /*for(int i = 0; i < 16; i++)
-    {
-      for(int j = 0; j < 16; j++)
-	{
-	   nMatchedToGenPho1 = 0;
-	   nMatchedToGenPho2 = 0;
-	   for(pfParticle = pfHandle->begin(); pfParticle != pfHandle->end(); pfParticle++)
-	    {
-	      
-	      DeltaR1 = deltaR(pfParticle->eta(),pfParticle->phi(),
-			       genHandle->begin()->eta(),genHandle->begin()->phi());
-	      
-	      DeltaR2 = deltaR(pfParticle->eta(),pfParticle->phi(),
-			       (genHandle->begin()+1)->eta(),(genHandle->begin()+1)->phi());
-	      
-	      if(DeltaR1 < DeltaR2) genParticle = genHandle->begin();
-	      else genParticle = (genHandle->begin()+1);
-	      
-	      if(pfParticle->pdgId() == 22)
-		{
-		  if( i == 0 && j==0) 
-		    {
-		      chrisHistos_->fill1DHistogram("PhotonDeltaR", deltaR(pfParticle->eta(),pfParticle->phi(), genParticle->eta(), genParticle->phi()));
-		      chrisHistos_->fill1DHistogram("PhotonDeltaPt", abs(pfParticle->pt() - genParticle->pt()));
-		      chrisHistos_->fill1DHistogram("PhotonDeltaEta", abs(pfParticle->eta() - genParticle->eta()));
-		      chrisHistos_->fill1DHistogram("PhotonDeltaPhi", abs(pfParticle->phi() - genParticle->phi()));
-		    }
-		  if(isMatched(pfParticle,genParticle, delR[i],delP[j]))
-		    {
-		      nInCutPhotons_[i][j]++;
-		      if(genParticle == genHandle->begin()) nMatchedToGenPho1++;
-		      else nMatchedToGenPho2++;
-		    }
-		}
-	    }
-	  if(nMatchedToGenPho1 != 0 && nMatchedToGenPho2 != 0) totPure_[i][j] += (1/nMatchedToGenPho1 + 1/nMatchedToGenPho2);
-	}
-    }
-  
-  */
-  
-
-
-  //counts the total number of true photons. I do this because its not quite 
-  //always 2000 since some of generated events can fail
- 
-  for(genParticle = genHandle->begin(); genParticle != genHandle->end(); genParticle++)
+  //Counts the number of generated photons (since sometimes events fail).
+  for(genParticle = genHandle->begin(); 
+      genParticle != genHandle->end(); genParticle++)
     {
       nTruePhotons_++;
     }
-      
+  
   return true;
 }
 
 
-//FLORIAN 
+
 double ChrisAnalyzer::transverseMass( const reco::Candidate::LorentzVector& lepton, 
 				      const reco::Candidate::LorentzVector& met) const {
   using namespace reco;
@@ -342,7 +365,63 @@ bool ChrisAnalyzer::isMatched(const PFCandidateCollection::const_iterator&  pfca
   
 }
 
+bool ChrisAnalyzer::isMatched(const PhotonCollection::const_iterator& recopho,
+			      const GenParticleCollection::const_iterator& gencand, 
+			      double& dr, double& dp)
+{
+  bool verbose =false;  
+  double gencandeta =gencand->eta();
+  double gencandphi =gencand->phi();
+  double recophoeta =recopho->eta();
+  double recophophi =recopho->phi();
+  double recophopt = recopho->pt();
+  double gencandpt = gencand->pt();
 
+  if(deltaR(recophoeta ,recophophi ,gencandeta,gencandphi) < dr &&
+     abs(recophopt-gencandpt) < dp) verbose =true;
   
+  return verbose;
+  
+}
+
+bool ChrisAnalyzer::isMatched(const PFCandidateCollection::const_iterator& pfcand,
+			      const GenParticleCollection::const_iterator& gencand, 
+			      double& deta, double& dphi, double& dp)
 
 
+{
+  if(gencand->pdgId() != 22) return false;
+  bool verbose =false;
+  double gencandeta =gencand->eta();
+  double gencandphi =gencand->phi();
+  double gencandpt = gencand->pt();
+  double pfcandeta =pfcand->eta();
+  double pfcandphi =pfcand->phi();
+  double pfcandpt = pfcand->pt();
+
+  if(abs(gencandeta - pfcandeta) < deta && 
+     abs(gencandphi - pfcandphi) < dphi &&
+     abs(gencandpt - pfcandpt) < dp) verbose =true;
+  return verbose;
+}
+
+bool ChrisAnalyzer::isMatched(const PhotonCollection::const_iterator& recopho,
+			      const GenParticleCollection::const_iterator& gencand, 
+			      double& deta, double& dphi, double& dp)
+
+
+{
+  if(gencand->pdgId() != 22) return false;
+  bool verbose =false;
+  double gencandeta =gencand->eta();
+  double gencandphi =gencand->phi();
+  double gencandpt = gencand->pt();
+  double recophoeta =recopho->eta();
+  double recophophi =recopho->phi();
+  double recophopt = recopho->pt();
+
+  if(abs(gencandeta - recophoeta) < deta && 
+     abs(gencandphi - recophophi) < dphi &&
+     abs(gencandpt - recophopt) < dp) verbose =true;
+  return verbose;
+}
--- ChrisAnalysis/src/ChrisHistograms.cc
+++ ChrisAnalysis/src/ChrisHistograms.cc
@@ -27,34 +27,46 @@ void ChrisHistograms::defineHistograms(){
 
    ///Here we define 1D histogram. The histograms are assigned to the TDirectory 
    /// pointed by the file argument 
-   
-   
-   //   add1DHistogram("pDEta","; Photon Delta Eta; Evenes", 20, -2.5,2.5,file_);
-   //   add1DHistogram("pDPhi","; Photon Delta Phi; Events", 20, -3.14,3.14,file_);
-   
-   add1DHistogram("inConePhotons", "; Number of Photons in the cone", 5,-.5,4.5, file_);
-   add1DHistogram("inConeElectrons", "Number of Electrons in the cone",5,-.5,4.5, file_);
-   add1DHistogram("inConePions", "Number of Pions in the cone",5,-.5,4.5, file_);
-   add1DHistogram("inConeKL0s", "Number of KL0s in the cone",10,-.5,9.5, file_);
-   add1DHistogram("inConePFParticles", "Number of PFParticles in the cone",20,-.5,19.5, file_);
+         
 
-      add1DHistogram("inConePhotonPt", " Photon pt distribution in the cone",40,-.5,39.5, file_);
-   add1DHistogram("inConeElectronPt", "Electron pt distribution in the cone",40,-.5,39.5, file_);
-   add1DHistogram("inConePionPt", "Pion pt distribution in the cone",40,-.5,39.5, file_);
-   add1DHistogram("inConeKL0Pt",    "KL0 pt distribution in the cone",40,-.5,39.5, file_);
-   add1DHistogram("inConePFParticlePt",    " PF Particle pt distribution in the cone",100,-.5,39.5, file_);
 
-   add1DHistogram("inConeTotPhotonPt", "Total pt from photons in cone",200,-.5,39.5, file_);
-   add1DHistogram("inConeTotElectronPt", "Total pt from electrons in cone",40,-.5,39.5, file_);
-   add1DHistogram("inConeTotPionPt", "Total pt from pions in cone",200,-.5,39.5, file_);
-   add1DHistogram("inConeTotKL0Pt",    "Total pt from KL0s in cone",200,-.5,39.5, file_);
-   add1DHistogram("inConeTotPFParticlePt",    "Total pt from PF Particles in cone",200,-.5,49.5, file_);
+   add1DHistogram("inConePhotons", "; Number of PF Photons in the cone", 5,0 ,5, file_);
+   add1DHistogram("inConeElectrons", "Number of Electrons in the cone",5, 0, 5, file_);
+   add1DHistogram("inConePions", "Number of Pions in the cone",5,0 , 5, file_);
+   add1DHistogram("inConeKL0s", "Number of KL0s in the cone",10,0 ,10, file_);
+   add1DHistogram("inConePFParticles", "Number of PFParticles in the cone",20,0, 20, file_);
+   add1DHistogram("inConeRecoPhotons", "; Number of Reco Photons in the cone", 5,0 ,5, file_); 
+
+   add1DHistogram("inConeTotPhotonPt", "Total pt from photons in cone",200,0,60, file_);
+   add1DHistogram("inConeTotElectronPt", "Total pt from electrons in cone",60, 0, 50, file_);
+   add1DHistogram("inConeTotPionPt", "Total pt from pions in cone",200,0, 60, file_);
+   add1DHistogram("inConeTotKL0Pt",    "Total pt from KL0s in cone",200, 0, 60, file_);
+   add1DHistogram("inConeTotPFParticlePt",    "Total pt from PF Particles in cone",200, 0, 60, file_);
+   add1DHistogram("inConeTotRecoPhotonPt",    "Total pt from reco photons in cone",200, 0, 60, file_);
+
+   add1DHistogram("photonOverMeasured", "Photon distribution when total Pt>38",50,0,50, file_);
+   add1DHistogram("electronOverMeasured", "Electron distribution when total Pt>38",50,0,50, file_);
+   add1DHistogram("pionOverMeasured", "Pion distribution when total Pt>38",50,0,50, file_);
+   add1DHistogram("KL0OverMeasured", "KL0 distribution when total Pt>38",50,0,50, file_);
    
-   add2DHistogram("inConePhotonDeltaAngle", "Delta Eta and Delta Phi Distribution of photons in the cone",20, -0.5, .5, 20, -.5, .5, file_); 
-   add2DHistogram("inConeElectronDeltaAngle", "Delta Eta and Delta Phi Distribution of electrons in the cone",20, -.5, .5, 20, -.5, .5, file_); 
-   add2DHistogram("inConePionDeltaAngle", "Delta Eta and Delta Phi Distribution of pions in the cone",20, -1, 1, 20, -1, 1, file_); 
-   add2DHistogram("inConeKL0DeltaAngle", "Delta Eta and Delta Phi Distribution of KL0s in the cone",20, -1, 1, 20, -1, 1, file_); 
-   add2DHistogram("inConePFParticleDeltaAngle", "Delta Eta and Delta Phi Distribution of pf particles in the cone",20, -1, 1, 20, -1, 1, file_); 
+   add1DHistogram("photonUnderMeasured", "Photon distribution when total Pt<32",50,0,40, file_);
+   add1DHistogram("electronUnderMeasured", "Electron distribution when total Pt<32",50,0,40, file_);
+   add1DHistogram("pionUnderMeasured", "Pion distribution when total Pt<32",50,0,40, file_);
+   add1DHistogram("KL0UnderMeasured", "KL0 distribution when total Pt<32",50,0,40, file_);
+   
+   add1DHistogram("photonCorrectlyMeasured", "Photon distribution when total Pt>3 and Pt<38",50,0,38, file_);
+   add1DHistogram("electronCorrectlyMeasured", "Electron distribution when total Pt>32 and Pt<38",50,0,38, file_);
+   add1DHistogram("pionCorrectlyMeasured", "Pion distribution when total Pt>32 and Pt<38",50,0,38, file_);
+   add1DHistogram("KL0CorrectlyMeasured", "KL0 distribution when total Pt>32 and Pt<38",50,0,38, file_);
+   
+
+
+
+   add2DHistogram("inConePhotonDeltaAngle", "Delta Eta and Delta Phi Distribution of photons in the cone",40, -0.1, .1, 80, -.5, .5, file_); 
+   add2DHistogram("inConeElectronDeltaAngle", "Delta Eta and Delta Phi Distribution of electrons in the cone",40, -.05, .05, 40, -.15, .15, file_); 
+   add2DHistogram("inConePionDeltaAngle", "Delta Eta and Delta Phi Distribution of pions in the cone",80, -.2, .2, 40, -1, 1, file_); 
+   add2DHistogram("inConeKL0DeltaAngle", "Delta Eta and Delta Phi Distribution of KL0s in the cone",40, -1, 1, 40, -1, 1, file_); 
+   add2DHistogram("inConePFParticleDeltaAngle", "Delta Eta and Delta Phi Distribution of pf particles in the cone",80, -1, 1, 20, -1, 1, file_); 
 
    
 
