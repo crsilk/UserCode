e059617e45e7f78cd355b2804dbc99e95d027e41
crsilk <>
2010-08-10 15:46:48 +0000

    Took out all of the unecessary commented lines

--- ChrisAnalysis/bin/chrisAnalysis.cc
+++ ChrisAnalysis/bin/chrisAnalysis.cc
@@ -46,41 +46,6 @@ int main(int argc, char ** argv){
   printf("RealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
   printf("%4.2f events / RealTime second .\n", nEventsAnalyzed/rtime);
   printf("%4.2f events / CpuTime second .\n", nEventsAnalyzed/ctime);
-  
-  TFile* FP = new TFile("delRdelP.root", "CREATE","delRdelP");
-
-  TH2D* PhoEff = new TH2D("PhoEff", "Photon Efficiency", 16, .05, .8, 16, .75, 12); 
-  TH2D* PhoPure = new TH2D("PhoPure", "Photon 'Purity'", 16, .05, .8, 16, .75, 12); 
-  TH2D* PhoQual = new TH2D("PhoQual", "Photon Quality" , 16, .05, .8, 16, .75, 12); 
-  
-  
-  //TH1D* PFFake = (TH1D*)FP->Get("PFFake");
-  //TH1D* KL0Fake = (TH1D*)FP->Get("KL0Fake");
-  //TH1D* PionFake = (TH1D*)FP->Get("PionFake");
-  //TH1D* EleFake = (TH1D*)FP->Get("EleFake");
-
-  /*double PT = myAnalyzers[0]->getPt();
-  PT = PT+.5;
-  int pt = (int)PT;
-  */
-  double phoqual;
-    
-  for( int i = 0; i < 16; i++)
-    {
-      for( int j = 0; j <16; j++)
-	{
-	  phoqual = sqrt(myAnalyzers[0]->getPhotonEfficiency(i,j)*myAnalyzers[0]->getPhotonPurity( i, j));
-	  PhoEff->SetBinContent(i, j,  myAnalyzers[0]->getPhotonEfficiency(i, j));
-	  PhoPure->SetBinContent(i, j, myAnalyzers[0]->getPhotonPurity(i, j));
-	  PhoQual->SetBinContent(i, j, phoqual);
-	}
-    }
-  //PFFake->SetBinContent(pt/10, myAnalyzers[0]->getWrongPFMatchRate());
-  //KL0Fake->SetBinContent(pt/10, myAnalyzers[0]->getKL0FakeRate());
-  //PionFake->SetBinContent(pt/10, myAnalyzers[0]->getPionFakeRate());
-  //EleFake->SetBinContent(pt/10, myAnalyzers[0]->getEleFakeRate());
-
-  FP ->Write();
     
   delete tree;
       
