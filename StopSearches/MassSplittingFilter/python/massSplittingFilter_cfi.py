acdea6027668584154f7c465045145e85bd4f59c
crsilk <>
2011-11-10 22:21:16 +0000

    removed some files

--- StopSearches/MassSplittingFilter/python/massSplittingFilter_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-massSplittingFilter = cms.EDFilter('MassSplittingFilter',
-					  source = cms.InputTag("source"),
-					  modelTag = cms.string("T2tt"),
-					  minSplitting = cms.double(0),
-					  maxSplitting = cms.double(175)
-)
