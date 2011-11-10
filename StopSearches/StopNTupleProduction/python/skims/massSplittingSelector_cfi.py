587fb62a85399b5a7f2232d662b6b4d7afe3f82e
crsilk <>
2011-11-10 21:53:18 +0000

    Took out Mass Splitting file and added the capability to ModelSelector

--- StopSearches/StopNTupleProduction/python/skims/massSplittingSelector_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-massSplittingSelector = cms.EDFilter('MassSplittingSelector',
-					  source = cms.InputTag("source"),
-					  modelTag = cms.string(""),
-					  minSplitting = cms.double(-999999999),
-					  maxSplitting = cms.double(999999999)
-)
