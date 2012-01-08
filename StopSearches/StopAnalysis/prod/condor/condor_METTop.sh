76c2d6cc7248b03f2f84b824b5d191f7f032ef97
crsilk <>
2012-01-08 19:27:46 +0000

    added condor files

---
+++ StopSearches/StopAnalysis/prod/condor/condor_METTop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+date
+
+source /uscmst1/prod/sw/cms/shrc prod
+
+workerspace=$PWD
+cd $CMSSW/src/
+eval `scramv1 runtime -sh`
+cd $workerspace
+
+
+cp $CMSSW/src/Analysis/StopAnalysis/prod/$CFG .
+cmsRun $CFG 
+
+cp *root $CMSSW/src/Analysis/StopAnalysis/data/
+
+rm *root
+rm *py
+
+
+date
+pwd
+#---------------------------------------------------------------------------------
