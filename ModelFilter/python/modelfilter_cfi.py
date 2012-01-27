e791d372d6229af523e77ca66c7fcc7f626ebe45
crsilk <>
2012-01-27 20:11:27 +0000

    Change it to modelfilter rather than selector

--- ModelFilter/python/modelfilter_cfi.py
+++ ModelFilter/python/modelfilter_cfi.py
@@ -1,8 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-modelSelector = cms.EDFilter('ModelSelector',
-					  source = cms.InputTag("source"),
-					  modelTag = cms.string("T2tt"),
-					  parameterMins = cms.vdouble(-99999999,-99999999),
-					  parameterMaxs = cms.vdouble(99999999, 99999999)
+modelfilter = cms.EDFilter('ModelFilter',
+    source = cms.InputTag("source"),
+    modelTag = cms.string("T2tt"),
+    selectOnRange = cms.bool(False),
+    selectOnMassSplitting = cms.bool(False),
+    MinSplitting = cms.double(0),
+    maxSplitting = cms.double(7000),
+    parameterMins = cms.vdouble(-99999999,-99999999),
+    parameterMaxs = cms.vdouble(99999999, 99999999)
 )
