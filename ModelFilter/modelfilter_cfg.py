6814483c5d28a15f9ae66f46686467b7904bc7bf
crsilk <>
2012-04-30 22:50:14 +0000

    *** empty log message ***

--- ModelFilter/modelfilter_cfg.py
+++ ModelFilter/modelfilter_cfg.py
@@ -3,36 +3,32 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("ModelFilter")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load("UserCode.ModelFilter.modelFilter_cfi")
+process.load("UserCode.ModelFilter.modelfilter_cfi")
 process.load("FastSimulation.Configuration.EventContent_cff")
 
 readFiles = cms.untracked.vstring()
-#readFiles.extend()
-
-
 
 
 process.source = cms.Source("PoolSource",
-#    fileNames = readFiles
-	 fileNames = cms.untracked.vstring("file:/uscmst1b_scratch/lpc1/3DayLifetime/crsilk/temp/00A9721F-44CB-E011-A65A-002618943869.root")
+	 fileNames = cms.untracked.vstring("dcap:///pnfs/cms/WAX/11/store/mc/Summer11/SMS-T1tttt_Mgluino-450to1200_mLSP-50to800_7TeV-Pythia6Z/AODSIM/PU_START42_V11_FSIM-v2/0001/FE8EB85A-A1F9-E011-BC2D-0025902009B0.root")
 )
 
-#process.load("CMGTools.Common.sources.SMS_T2tt_Mstop_225to1200_mLSP_50to1025_7TeV_Pythia6Z.Summer11_PU_START42_V11_FastSim_v1.AODSIM.source_cff")
 
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-
-process.modelFilter.modelTag = cms.string("T2tt")
-process.modelFilter.modelParameters = cms.vstring("700.0","450.0")
+process.modelfilter.modelTag = cms.string("T1tttt")
+process.modelfilter.selectOnRange = cms.bool(True)
+process.modelfilter.parameterMins = cms.vdouble(0.0, 600, 175)
+process.modelfilter.parameterMaxs = cms.vdouble(0.0, 600, 175)
 
 process.aod = cms.OutputModule("PoolOutputModule",
      process.AODSIMEventContent,
-     fileName = cms.untracked.string('aod.root'),
-	 SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('modelFilter_step'))
+     fileName = cms.untracked.string('T1tttt_test.root'),
+	 SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('modelfilter_step'))
  )
 
 process.aod_step = cms.EndPath(process.aod)
-process.modelFilter_step = cms.Path(process.modelFilter)
+process.modelfilter_step = cms.Path(process.modelfilter)
 
-process.schedule =cms.Schedule(process.modelFilter_step, process.aod_step)
+process.schedule =cms.Schedule(process.modelfilter_step, process.aod_step)
